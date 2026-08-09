c8f42a898522c003c9242976013e62c5a44f8045
Or <ortabibi2002@gmail.com>
2026-08-10 00:51:51 +0300

    test

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,24 @@
 
-export function MailIndex() {
-    return <section className="container">Mail app</section>
-}
+const { useState, useEffect } = React
+const { Link, useSearchParams } = ReactRouterDOM
 
+import { mailService } from '../services/mail.service.js'
+import { utilService } from '../services/util.service.js'
+
+export function MailIndex() {
+    const [mails, setMails] = useState(null)
+
+
+    useEffect(() => {
+        loadMails(mails)
+    }, [mails])
+
+    function loadMails() {
+        mailService.query(filterBy).then(setMails)
+    }
+
+
+
+
+    return <section className="container">Mail appppp</section>
+}
