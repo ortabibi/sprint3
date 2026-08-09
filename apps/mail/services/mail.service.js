ffb3249f195cc8c4eaddbf307b18b63e10f9fe3b
Or <ortabibi2002@gmail.com>
2026-08-10 00:08:15 +0300

    test

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,32 @@
-// mail service
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+const MAIL_KEY = 'mailDB'
+// _createCars()
+
+export const mailService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyCar,
+    getDefaultFilter,
+    getSpeedStats,
+    getVendorStats,
+    getFilterFromSearchParams,
+}
+
+function query(filterBy = {}) {
+    return storageService.query(CAR_KEY)
+        .then(mails => {
+            if (filterBy.txt) {
+                const regExp = new RegExp(filterBy.txt, 'i')
+                mails = mails.filter(mail => regExp.test(mail.vendor))
+            }
+
+            if (filterBy.minSpeed) {
+                mails = mails.filter(mail => mail.maxSpeed >= filterBy.minSpeed)
+            }
+
+            return mails
+        })
+}
