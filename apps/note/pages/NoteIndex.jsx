6671faf7fd5cdcb899d0d13047c4a85eb16c83e7
S K <soli.kolet@email.com>
2026-08-10 12:36:08 +0300

    add index

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,44 @@
-
 export function NoteIndex() {
-    return <section className="container">Notes app</section>
+  const [notes, setNotes] = useState(null)
+
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [filterBy, setFilterBy] = useState(
+    noteService.getFilterFromSearchParams(searchParams),
+  )
+
+  useEffect(() => {
+    loadNotes(filterBy)
+  }, [])
+
+  useeffectUpdate(() => {
+    loadNotes(filterBy)
+    setSearchParams(noteService.trimObj(filterBy))
+  }, [filterBy])
+
+  function loadNotes() {
+    noteService.query(filterBy).then(setNotes)
+  }
+  function onRemoveNote(noteId) {
+    noteService
+      .remove(noteId)
+      .then(() => {
+        setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId))
+        onClearFilter()
+        showSuccessMsg(`Note ${noteId} removed`)
+      })
+      .catch((err) => showErrorMsg(`Cannot remove ${noteId}`))
+  }
+
+  function onClearFilter() {
+    setFilterBy(carService.getDefaultFilter())
+  }
+
+  if (!notes)
+    return (
+      <div className="loader">
+        <img src="/assets/loader.gif" alt="Loading..." />
+      </div>
+    )
+
+  return <section className="container">Notes app</section>
 }
