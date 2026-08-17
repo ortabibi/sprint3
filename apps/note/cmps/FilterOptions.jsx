a44cff40b2364f7ea543634f455162ec79252ba7
S K <soli.kolet@email.com>
2026-08-18 01:47:51 +0300

    css: added css to noteFilter

--- apps/note/cmps/FilterOptions.jsx
+++ apps/note/cmps/FilterOptions.jsx
@@ -1,41 +1,38 @@
-const useState = React.useState
+const { useState } = React
 export function FilterOptions({ filterBy, onSetFilterBy,onSelectType,}) {
-
-    const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
-    
     return <section className="filter-options">
     <div className="filter-options-header">
     <span>Types</span>
    <button type="button" className="clear-filter-btn" onClick={() => onSelectType('')}>
   Clear
 </button>
+</div>
 
     <div className="option-types">
-    <div className="images-option"onClick={() => onSelectType('NoteImg')} >
+        
+    <div className="option-card"onClick={() => onSelectType('NoteImg')} >
         <i className="fa-regular fa-image" aria-hidden="true" ></i>
         <span>Images</span>
     </div>
 
-    <div className="videos-option" onClick={() => onSelectType('NoteVideo')}>
+    <div className="option-card" onClick={() => onSelectType('NoteVideo')}>
         <i className="fa-brands fa-youtube" aria-hidden="true"></i>
         <span>Videos</span>
     </div>
 
-    <div className="videos-option" onClick={() => onSelectType('NoteTodos')}>
+    <div className="option-card" onClick={() => onSelectType('NoteTodos')}>
         <i className="fa-regular fa-square-check" aria-hidden="true"></i>
         <span>Todos</span>
     </div>
 
-    <div className="videos-option" onClick={() => onSelectType('NoteTxt')}>
+    <div className="option-card" onClick={() => onSelectType('NoteTxt')}>
         <i className="fa-solid fa-pencil" aria-hidden="true" ></i>
         <span>Texts</span>
     </div>
-     <div className="images-option"onClick={() => onSelectType('NoteCanvas')} >
+     <div className="option-card "onClick={() => onSelectType('NoteCanvas')} >
         <i className="fa-solid fa-paintbrush" aria-hidden="true" ></i>
         <span>Canvas</span>
     </div>
 </div>
-</div>
-
   </section>
 }
