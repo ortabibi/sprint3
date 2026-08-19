978b6e81838cda1968199a670dbcf0a1bde3866a
Or <ortabibi2002@gmail.com>
2026-08-20 00:19:06 +0300

    done email

--- pages/About.jsx
+++ pages/About.jsx
@@ -11,7 +11,7 @@ export function About() {
         <section className="about">
             <div className="animation-container">
                 <video
-                    src="assets/animation/fist-bump.mp4"
+                    src="assets/css/animation/fist-bump.mp4"
                     width="200"
                     autoPlay
                 // controls
@@ -20,40 +20,40 @@ export function About() {
             </div>
 
             <div className="team-lead">
-                <div className="profile-img"><img src="assets/img/team-batel.png" /></div>
+                <div className="profile-img"><img src="assets/css/img/team-gal.png.jpg" /></div>
 
                 <ul className="contact-info">
-                    <h2>Batel Katiei</h2>
+                    <h2>Gal Wender</h2>
                     <h3>Team Lead</h3>
-                    <li> <i className="fa-regular fa-envelope"></i><span>batelkatiei1@gmail.com</span></li>
+                    <li> <i className="fa-regular fa-envelope"></i><span>galwender.dev@gmail.com</span></li>
                 </ul>
             </div>
 
             <article className="team-data">
                 <div className="team-member">
                     <div className="team-member-description">
-                        <h2>Matan Odentz</h2>
+                        <h2>Or Tabibi</h2>
                         <h3>App Developer</h3>
                     </div>
-                    <div className="profile-img"><img src="assets/img/team-matan.png" /></div>
+                    <div className="profile-img"><img src="assets/css/img/team-or.png.jpeg" /></div>
                     <ul className="contact-info">
                         <hr />
-                        <li> <i className="fa-solid fa-phone"></i><span>+972-54-445-2430</span></li>
-                        <li> <i className="fa-regular fa-envelope"></i><span>matanodentz@gmail.com</span></li>
+                        <li> <i className="fa-solid fa-phone"></i><span>+972-54-224-8422</span></li>
+                        <li> <i className="fa-regular fa-envelope"></i><span>ortabibi2002@gmail.com</span></li>
                     </ul>
                 </div>
 
                 <div className="team-member">
                     <div className="team-member-description">
-                        <h2>Gal Ben David</h2>
+                        <h2>Soli Kolet</h2>
                         <h3>App Developer</h3>
                     </div>
-                    <div className="profile-img"><img src="assets/img/team-gal.png" /></div>
+                    <div className="profile-img"><img src="assets/css/img/team-soli.png.jpeg" /></div>
 
                     <ul className="contact-info">
                         <hr />
-                        <li> <i className="fa-solid fa-phone"></i><span>+972-52-429-4752</span></li>
-                        <li>  <i className="fa-regular fa-envelope"></i><span>gal.benda3@gmail.com</span></li>
+                        <li> <i className="fa-solid fa-phone"></i><span>+972-54-611-6662</span></li>
+                        <li>  <i className="fa-regular fa-envelope"></i><span>soli.kolet@gmail.com</span></li>
                     </ul>
                 </div>
 
